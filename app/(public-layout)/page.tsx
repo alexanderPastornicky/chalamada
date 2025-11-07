3343f61e20748ff9c05c92fa4187c1feb7a7c67c
Alexander Pastornicky <alexander.pastornicky@chiaralabs.com>
2025-11-07 19:23:47 +0100

    Update homepage content: changed "project management" to "time management" for improved clarity.

--- app/(public-layout)/page.tsx
+++ app/(public-layout)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="space-y-2">
         <p className="text-foreground text-2xl md:text-4xl tracking-tight">
           <span className="font-thin text-muted-foreground">Lightweight </span>
-          <span className="font-light">project management</span>
+          <span className="font-light">time management</span>
         </p>
         <div className="flex justify-end">
           <Button>Get started</Button>
